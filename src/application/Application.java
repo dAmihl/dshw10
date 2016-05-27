969bb1a22a7f9989366d7bd533c19fa930df287a
dAmihl <csaq1182@student.uibk.ac.at>
2016-05-27 16:06:08 +0200

    added simple AES encryption

--- src/application/Application.java
+++ src/application/Application.java
@@ -4,7 +4,7 @@ import client.EncryptionClient;
 
 public class Application {
 
-	private static String KEY = "DATSECUREKEY";
+	private static String KEY = "thisIsASecretKey";
 	private static Integer PORT = 1337;
 	private static boolean bRunAsServer = true;
 	
@@ -13,13 +13,22 @@ public class Application {
 			System.out.println("No key given as argument. Using standard key '"+KEY+"'");
 			
 		}else{
-			KEY = args[0];
+			if (args[0].toLowerCase().equals("client")){
+				bRunAsServer = false;
+			}else{
+				KEY = args[0];
+			}
 		}
 		
 		if (args.length > 1){
 			if (args[1].equals("client")){
 				bRunAsServer = false;
 			}
+		}
+		
+		if (KEY.length() != 16){
+			System.out.println("Wrong key length: Needed length 16.");
+			System.exit(-1);
 		}
 		
 		new EncryptionClient(KEY, PORT, bRunAsServer);
--- src/client/EncryptionClient.java
+++ src/client/EncryptionClient.java
@@ -3,10 +3,21 @@ package client;
 import java.io.IOException;
 
 import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
 import java.util.Scanner;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.SecretKeySpec;
 
 
 
@@ -117,11 +128,48 @@ public class EncryptionClient {
 	
 	
 	private String encryptMessage(String msg){
-		return msg;
+		Cipher cipher;
+		String encryptedString ="";
+		try {
+			cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
+			final SecretKeySpec secretKey = new SecretKeySpec(key.getBytes(), "AES");
+		    cipher.init(Cipher.ENCRYPT_MODE, secretKey);
+		    encryptedString = new String(Base64.getEncoder().encode(cipher.doFinal(msg.getBytes())));
+		} catch (NoSuchAlgorithmException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (NoSuchPaddingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InvalidKeyException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalBlockSizeException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (BadPaddingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	    return encryptedString;
+
 	}
 	
 	private String decryptMessage(String secretMessage){
-		return secretMessage;
+		try
+        {
+            Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5PADDING");
+            final SecretKeySpec secretKey = new SecretKeySpec(key.getBytes(), "AES");
+            cipher.init(Cipher.DECRYPT_MODE, secretKey);
+            final String decryptedString = new String(cipher.doFinal(Base64.getDecoder().decode(secretMessage)));
+            return decryptedString;
+        }
+        catch (Exception e)
+        {
+          e.printStackTrace();
+
+        }
+        return null;
 	}
 	
 	
